35381fad0f1a9d4b7e6d556e996d2095082b37a6
Aliaksei Sakalou <nullbsd@gmail.com>
2019-12-09 11:10:37 +0300

    new address

--- wiki/lib/exe/opensearch.php
+++ wiki/lib/exe/opensearch.php
@@ -1,7 +1,7 @@
 <?xml version="1.0"?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
   <ShortName>TrueBSD Project</ShortName>
-  <Image width="16" height="16" type="image/x-icon">http://truebsd.pw/wiki/lib/tpl/default/images/favicon.ico</Image>
-  <Url type="text/html" template="http://truebsd.pw/wiki/doku.php?do=search&amp;id={searchTerms}" />
-  <Url type="application/x-suggestions+json" template="http://truebsd.pw/wiki/lib/exe/ajax.php?call=suggestions&amp;q={searchTerms}" />
+  <Image width="16" height="16" type="image/x-icon">https://truebsdorg.github.io/wiki/lib/tpl/default/images/favicon.ico</Image>
+  <Url type="text/html" template="https://truebsdorg.github.io/wiki/doku.php?do=search&amp;id={searchTerms}" />
+  <Url type="application/x-suggestions+json" template="https://truebsdorg.github.io/wiki/lib/exe/ajax.php?call=suggestions&amp;q={searchTerms}" />
 </OpenSearchDescription>
