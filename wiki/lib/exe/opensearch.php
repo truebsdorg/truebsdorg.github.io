ab7425240a5d8b8e4d9ef8eaf9fda2879092f811
Your Name <you@example.com>
2025-12-25 23:48:12 +0400

    new

---
+++ wiki/lib/exe/opensearch.php
@@ -0,0 +1,7 @@
+<?xml version="1.0"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+  <ShortName>TrueBSD Project</ShortName>
+  <Image width="16" height="16" type="image/x-icon">https://truebsdorg.github.io/wiki/lib/tpl/default/images/favicon.ico</Image>
+  <Url type="text/html" template="https://truebsdorg.github.io/wiki/doku.php?do=search&amp;id={searchTerms}" />
+  <Url type="application/x-suggestions+json" template="https://truebsdorg.github.io/wiki/lib/exe/ajax.php?call=suggestions&amp;q={searchTerms}" />
+</OpenSearchDescription>
